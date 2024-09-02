47fd6469f6bd571a0f224fa86a7c814b92e3b11f
RiccardoMamoli <riccardomamoli2@gmail.com>
2024-09-02 12:00:35 +0200

    compiling

--- Esercizi_Lesson_1/fs0624-epizon-redux-main/src/index.js
+++ Esercizi_Lesson_1/fs0624-epizon-redux-main/src/index.js
@@ -6,5 +6,12 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './style/index.css'
 
+import store from './redux/store'
+import { Provider } from 'react-redux'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+    //privder ha bisogno di una prop chiamata store e gli diamo come valore l'oggetto storre che abbiamo esportato prima
+    <Provider store={store}>
+        <App />
+    </Provider>)
